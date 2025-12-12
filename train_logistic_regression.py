a9bb8e7619358a6721716bcfb1dee5730413e94b
ignaciojimenezr <67474336+ignaciojimenezr@users.noreply.github.com>
2025-12-11 18:04:52 -0800

    comment

--- train_logistic_regression.py
+++ train_logistic_regression.py
@@ -20,6 +20,7 @@ param_grid = {
     'C': [0.1, 1, 10, 100]
 }
 
+# GridSearchCV to find the best hyperparameters
 grid_search = GridSearchCV(
     LogisticRegression(max_iter=1000, random_state=42),
     param_grid,
